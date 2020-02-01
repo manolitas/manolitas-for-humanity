84f4bc974fefcdf771e21b8cdc80e9ac2e0800d5
Carla Rodriguez <n298775@gruposantander.com>
2020-02-01 17:36:35 +0100

    create charts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import DataVisulization from "./components/dataVisualization/index.jsx";
+
 function App() {
   return (
     <div className="app">
+      <DataVisulization></DataVisulization>
     </div>
   );
 }
