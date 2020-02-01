c3afdfcbcb4bf9f15edce8012cf500ce804073e7
Carla Rodriguez <n298775@gruposantander.com>
2020-02-01 15:32:35 +0100

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+import Landing from "./components/landing/index.js";
+// import Charts from "./components/charts/index.jsx";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatbotOpen: false,
+    }
+
+    this.handleChatbot = this.handleChatbot.bind(this);
+  }
+  
+  handleChatbot() {
+    this.setState({chatbotOpen: !this.state.chatbotOpen})
+}
+
+  render() {
+    const { chatbotOpen } = this.state;
+    return (
+      <div className="app">
+        <Landing chatbotOpen={chatbotOpen} handleChatbot={this.handleChatbot}></Landing>
+        {/* <Charts></Charts> */}
+      </div>
+    );
+  }
 }
 
 export default App;
