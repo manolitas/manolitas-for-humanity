d2ec60857608c8b745ae38fe2430e36c03807300
Carlos Mañas <carlos@spacenomads.com>
2020-02-01 18:23:45 +0100

    Add production version

---
+++ docs/precache-manifest.89141709b7839358b2e879698fef69c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3677ccef89535b5c75fb48e32cc231e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb05c546be7d0e89b088",
+    "url": "./static/css/main.668ab2f2.chunk.css"
+  },
+  {
+    "revision": "bd874d1ba4d664ff08a9",
+    "url": "./static/js/2.663074c7.chunk.js"
+  },
+  {
+    "revision": "a22a7ae5fe503af7d2f4bea67c7372bc",
+    "url": "./static/js/2.663074c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb05c546be7d0e89b088",
+    "url": "./static/js/main.ecebaefc.chunk.js"
+  },
+  {
+    "revision": "0ff0b251f0b76e946ee7",
+    "url": "./static/js/runtime-main.e3161468.js"
+  },
+  {
+    "revision": "d8e266190c3e82d9a99f9509fd74ca23",
+    "url": "./static/media/Paco.d8e26619.svg"
+  },
+  {
+    "revision": "b8950f364e01567f25478f38b1a27f75",
+    "url": "./static/media/hero-luzon.b8950f36.png"
+  }
+]);
